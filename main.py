f9b3a0e84316841d406c4be532146ddba5135889
Freditansari <Freditansari@users.noreply.github.com>
2020-05-07 20:59:22 +0700

    added endpoints to accept telegram commands

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ received_commands=[]
 def home_view(): 
 	return "<h1>Hello world</h1>"
 
-@app.post('/telegram-commands', methods=['GET','POST'])
+@app.route('/telegram-commands', methods=['GET','POST'])
 def receive_commands():
     if request.method=='POST':
         received_commands.append(request.form)
