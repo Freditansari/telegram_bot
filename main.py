8cb0fc018318eb89f442ac39b826cfb2896efdab
Freditansari <Freditansari@users.noreply.github.com>
2020-05-07 20:49:54 +0700

    added endpoints to accept telegram commands

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from flask import Flask 
+from flask import Flask, request, jsonify
 
 app = Flask(__name__) 
 
+received_commands=[]
 @app.route("/") 
 def home_view(): 
 	return "<h1>Hello world</h1>"
+
+@app.post('/telegram-commands', methods=['GET','POST'])
+def receive_commands():
+    if request.method=='POST':
+        received_commands.append(request.form)
+        return 'ok'
+    return jsonify(received_commands)
+
 
 if __name__ =='__main__':
     app.secret_key='secret1234'
