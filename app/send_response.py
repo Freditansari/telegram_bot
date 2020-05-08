f4cb0412762a708a2406793afd762c2a36af011d
Freditansari <Freditansari@users.noreply.github.com>
2020-05-08 12:48:07 +0700

    fixed the non replying bot

--- app/send_response.py
+++ app/send_response.py
@@ -1,9 +1,10 @@
 import requests
-def send(bot_message):
+# todo does not send to other people only me
+def send(bot_message, chat_id):
 
-    bot_chatID = '559737247'
+    # bot_chatID = '559737247'
 
-    send_text = 'https://api.telegram.org/bot1270470028:AAGitNmo6dBYzfatJ2bokXHCfL_AuMFfiiY'+'/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+    send_text = 'https://api.telegram.org/bot1270470028:AAGitNmo6dBYzfatJ2bokXHCfL_AuMFfiiY'+'/sendMessage?chat_id=' + chat_id + '&parse_mode=Markdown&text=' + bot_message
 
     # resp = requests.post('https://api.telegram.org/bot{}/sendMessage'.format(token), params)
 
