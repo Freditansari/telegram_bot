f3483b880cfc15e21997b720806da78d41cc95b5
Freditansari <Freditansari@users.noreply.github.com>
2020-05-08 13:06:36 +0700

    fixed the non replying bot

--- app/send_response.py
+++ app/send_response.py
@@ -4,9 +4,9 @@ import sys
 def send(bot_message, chat_id):
 
     # bot_chatID = '559737247'
-    print(bot_message, + str(chat_id))
+    print(bot_message , chat_id)
     sys.stdout.flush()
-    send_text = 'https://api.telegram.org/bot1270470028:AAGitNmo6dBYzfatJ2bokXHCfL_AuMFfiiY'+'/sendMessage?chat_id=' + str(chat_id) + '&parse_mode=Markdown&text=' + bot_message
+    send_text = 'https://api.telegram.org/bot1270470028:AAGitNmo6dBYzfatJ2bokXHCfL_AuMFfiiY'+'/sendMessage?chat_id=' ,chat_id,  '&parse_mode=Markdown&text=' + bot_message
 
     # resp = requests.post('https://api.telegram.org/bot{}/sendMessage'.format(token), params)
 
